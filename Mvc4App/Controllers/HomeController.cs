41b7026a08da955025a0bd7199eb15d4bfd1b1a6
Young <liao.brave@gmail.com>
2015-11-20 13:39:10 +0800

    Update HomeController.cs
    modify something in the controller for a test

--- Mvc4App/Controllers/HomeController.cs
+++ Mvc4App/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Mvc4App.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "This is amazing,haha";
 
             return View();
         }
